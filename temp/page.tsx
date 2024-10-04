ee5d06438eac6508b9ac1c12cc8d8f51a4f5c893
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-10-04 23:30:38 +0500

    Add files via upload

---
+++ temp/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+// import Button from "./component/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-center">
+      Home Page
+    </h1>
+
+
+    </div>
+
+
+      );
+}
+
