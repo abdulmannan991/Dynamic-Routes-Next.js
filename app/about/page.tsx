9a3e5e990f977d2d4af6998decd2590829ac3cba
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-10-04 23:38:28 +0500

    Add files via upload

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function About() {
+  return (
+    <div>
+    <h1 className="text-center max-w-7xl mx-auto p-4 sm:p-6 md:p-8">
+    This is About Page
+    </h1>
+
+
+    </div>
+      );
+}
+
+
